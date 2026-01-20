22a3233687d4a7bf14143fbe2bb9f1ad32030ebb
Braden Borman <bradenborman@hotmail.com>
2026-01-19 21:29:02 -0600

    testing

--- server/src/main/java/com/vault/entity/AspectRatio.java
+++ server/src/main/java/com/vault/entity/AspectRatio.java
@@ -1,7 +1,23 @@
 package com.vault.entity;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonValue;
+
 public enum AspectRatio {
     SQUARE,
     PORTRAIT,
-    LANDSCAPE
+    LANDSCAPE;
+
+    @JsonCreator
+    public static AspectRatio fromString(String value) {
+        if (value == null) {
+            return null;
+        }
+        return AspectRatio.valueOf(value.toUpperCase());
+    }
+
+    @JsonValue
+    public String toValue() {
+        return this.name().toLowerCase();
+    }
 }
